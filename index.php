82790d4597f755456f2137522ab2ba36c238c182
suisrael <88337104+suisrael@users.noreply.github.com>
2024-12-24 10:17:34 -0500

    Push initial files from UF Dec 2024 Mercury theme

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+/*
+Plugin Name: Featured News Block
+Version: 0.1.0
+Author: The WordPress Contributors
+*/
+include_once(plugin_dir_path(__FILE__) . 'includes/custom-api-functions.php');
+include_once(plugin_dir_path(__FILE__) . 'includes/latest-news-block.php');
